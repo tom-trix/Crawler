3d287c852032a715e5839709153b9d0cc9898cd0
tom-trix <tom-trix@yandex.ru>
2012-12-02 14:30:03 +0600

    Изменил Кролёр

--- src/ru/tomtrix/dm/crawler/Crawler.scala
+++ src/ru/tomtrix/dm/crawler/Crawler.scala
@@ -27,6 +27,11 @@ object Crawler {
     /** Number of article till which we should crawl */
     val endArticle = 371500
 
+    def parse(i: Int, xpath: String) = try {
+        val x = XPathParser.getInstance.parseNodes("http://sport.rbc.ru/football/newsline/24/09/2012/" + i + ".shtml", xpath).toArray.toList map { _.asInstanceOf[DefaultElement].getText } filter (!_.trim.isEmpty)
+        if (x(0).startsWith("Ресурс, который вы запросили, не найден на сервере")) None else Some(x)
+    } catch { case e: Exception => None }
+
     def main(args: Array[String]): Unit = {
         //манипуляции с БД
         mongoDocs.remove(MongoDBObject.empty)
@@ -38,28 +43,24 @@ object Crawler {
         mongoWords.ensureIndex("word")
 
         //запускаем паука
+        val s = """//*[@id="window"]/div/div[2]/div[1]/div/div"""
+        val xpath = List("[2]/*", "[3]/*", "[4]") map (s + _)
         for {
             i <- startArticle to endArticle
-            s <- Some("""//*[@id="window"]/div/div[2]/div[1]/div/div""")
-            all <- try {
-                Some(for {
-                    //во 2-м диве содержится статья, в 3-м - теги, в 4-м - дата публикации
-                    xpath <- List("[2]/*", "[3]/*", "[4]") map (s + _)
-                    pr <- Some(XPathParser.getInstance.parseNodes("http://sport.rbc.ru/football/newsline/24/09/2012/" + i + ".shtml", xpath).toArray map { _.asInstanceOf[DefaultElement].getText } filter (!_.trim.isEmpty))
-                } yield pr)
-            } catch { case e: Exception => None }
-            docs <- if (all(0)(0).startsWith("Ресурс, который вы запросили, не найден на сервере")) None else Some(all)
-            date <- Some(docs(2)(0) split " ") 
+            docs <- parse(i, xpath(0))
+            tags <- parse(i, xpath(1))
+            prse <- parse(i, xpath(2))
+            date <- Some(prse(0) split (" "))
         } yield {
             //добавляем статью в коллекцию документов
-            mongoDocs.insert(MongoDBObject("article" -> i, "doc" -> docs(0), "tags" -> docs(1), "date" -> new Date(date(2).toInt - 1900, months indexOf stem(date(1)), date(0) toInt)))
+            mongoDocs.insert(MongoDBObject("article" -> i, "doc" -> docs, "tags" -> tags, "date" -> new Date(date(2).toInt - 1900, months indexOf stem(date(1)), date(0) toInt)))
             //строим обратный индекс для ключевых слов
             for {
-                keyword <- docs(1) map (_.trim toLowerCase) filter (!_.isEmpty)
+                keyword <- tags map (_.trim toLowerCase) filter (!_.isEmpty)
             } yield mongoKeywords.update(MongoDBObject("keyword" -> stem(keyword)), $addToSet("docs" -> i), true, false)
             //строим обратный индекс для всех слов
             for {
-                sentence <- docs(0)
+                sentence <- docs
                 word <- sentence.split(splitRegex) map (_.trim toLowerCase) filter (t => !t.isEmpty && !stopWords.contains(t))
             } yield mongoWords.update(MongoDBObject("word" -> stem(word)), $addToSet("docs" -> i), true, false)
         }
